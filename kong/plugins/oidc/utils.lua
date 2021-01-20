ab64cbac05b91ad3cd9364073b5d658275d0c55e
Marcelo <marinho.marcelo@gmail.com>
2021-01-20 16:30:23 -0300

    header

--- kong/plugins/oidc/utils.lua
+++ kong/plugins/oidc/utils.lua
@@ -187,6 +187,7 @@ function M.injectHeaders(header_names, header_claims, sources)
             end
         end
     end
+    kong.service.request.set_header("teste", "teste123")
 end
 
 function M.has_bearer_access_token()
