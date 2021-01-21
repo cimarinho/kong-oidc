126e11087d6a54239c74b2f22324d5b1cd6ee7ce
Marcelo <marinho.marcelo@gmail.com>
2021-01-21 11:18:27 -0300

    add header

--- kong/plugins/oidc/utils.lua
+++ kong/plugins/oidc/utils.lua
@@ -155,7 +155,9 @@ function M.injectHeaderByToken(accessToken, header_names)
 
     local payload2= jsonDes["payload"]["realm_access"]["roles"]
 
-    kong.log.info(payload2[1])
+    for i, value in ipairs(payload2) do
+        kong.log.info(value)
+    end
 
     for i, value in ipairs(header_names) do
         kong.log.info(value)
