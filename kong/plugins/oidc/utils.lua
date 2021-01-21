05a9f916e1f8c9a3693cefe20101bec561def462
Marcelo <marinho.marcelo@gmail.com>
2021-01-21 09:49:39 -0300

    add header

--- kong/plugins/oidc/utils.lua
+++ kong/plugins/oidc/utils.lua
@@ -144,7 +144,7 @@ function M.injectHeaderByToken(accessToken, header_names)
     local cjson = require "cjson"
     local jwt_obj = jwt:load_jwt(accessToken)
     kong.log.info(jwt_obj)
-    local json = cjson.encode(jwt_obj)
+    local json = cjson.decode(jwt_obj)
 
     kong.log.info(json)
     kong.log.info(json['signature'])
